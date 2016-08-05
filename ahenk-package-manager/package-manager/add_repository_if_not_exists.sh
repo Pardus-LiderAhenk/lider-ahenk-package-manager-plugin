203846c84fc5da1185b2173f6c9415681338f6c3
cemrealpsoy <cemrealpsoy@gmail.com>
2016-08-05 13:55:53 +0300

    Update will work every time in case  repos are not updated

--- ahenk-package-manager/package-manager/add_repository_if_not_exists.sh
+++ ahenk-package-manager/package-manager/add_repository_if_not_exists.sh
@@ -2,5 +2,5 @@
 
 if ! grep -q "$1" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
     echo $1 | sudo tee --append /etc/apt/sources.list.d/ahenk.list > /dev/null
-    sudo apt-get update
 fi
+    sudo apt-get update
