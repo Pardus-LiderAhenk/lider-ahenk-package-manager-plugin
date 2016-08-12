90190cf390a27ceccdb3fdfd1bea405c1fd5daa5
cemrealpsoy <cemrealpsoy@gmail.com>
2016-08-12 18:24:57 +0300

    new label is added because of new table column

--- lider-console-package-manager/src/tr/org/liderahenk/packagemanager/model/PackageCheckItem.java
+++ lider-console-package-manager/src/tr/org/liderahenk/packagemanager/model/PackageCheckItem.java
@@ -7,15 +7,17 @@ public class PackageCheckItem implements Serializable{
 	private static final long serialVersionUID = 5605455949309964919L;
 	
 	private String dn;
+	private String version;
 	private String result;
 
 	public PackageCheckItem() {
 		super();
 	}
 
-	public PackageCheckItem(String result, String uid) {
+	public PackageCheckItem(String result, String version, String uid) {
 		super();
 		this.result = result;
+		this.version = version;
 		this.dn = uid;
 	}
 
@@ -33,6 +35,14 @@ public class PackageCheckItem implements Serializable{
 
 	public void setDn(String uid) {
 		this.dn = uid;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
 	}
 	
 
