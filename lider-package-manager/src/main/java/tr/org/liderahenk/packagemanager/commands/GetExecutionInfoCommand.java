f3be3ea1e733ce9b5d3a799f5ed82ee72be3186d
emrekgn <emre.akkaya@agem.com.tr>
2016-12-07 15:12:40 +0300

    contribute to agent properties

--- lider-package-manager/src/main/java/tr/org/liderahenk/packagemanager/commands/GetExecutionInfoCommand.java
+++ lider-package-manager/src/main/java/tr/org/liderahenk/packagemanager/commands/GetExecutionInfoCommand.java
@@ -34,6 +34,7 @@ import tr.org.liderahenk.packagemanager.entities.CommandExecutionStatistics;
 import tr.org.liderahenk.packagemanager.entities.CommandPackageVersion;
 
 public class GetExecutionInfoCommand implements ICommand, ITaskAwareCommand {
+
 	private ICommandResultFactory resultFactory;
 	private IPluginInfo pluginInfo;
 	private IPluginDbService pluginDbService;
@@ -44,13 +45,82 @@ public class GetExecutionInfoCommand implements ICommand, ITaskAwareCommand {
 
 	@Override
 	public ICommandResult execute(ICommandContext context) {
-
 		return resultFactory.create(CommandResultStatus.OK, new ArrayList<String>(), this);
 	}
 
 	@Override
 	public ICommandResult validate(ICommandContext context) {
-		return resultFactory.create(CommandResultStatus.OK, null, this, null);
+		return resultFactory.create(CommandResultStatus.OK, null, this);
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public void onTaskUpdate(ICommandExecutionResult result) {
+
+		ICommandExecutionResult res = getCommandDao().findExecutionResult(result.getId());
+		byte[] data = null;
+		if (res != null)
+			data = res.getResponseData();
+		Map<String, Object> responseData;
+		try {
+			responseData = new ObjectMapper().readValue(data, 0, data.length,
+					new TypeReference<HashMap<String, Object>>() {
+					});
+			if (responseData != null && !responseData.isEmpty() && responseData.containsKey("commandExecutionInfoList")
+					&& responseData.containsKey("versionList")) {
+				Object object = responseData.get("commandExecutionInfoList");
+				Object versionObject = responseData.get("versionList");
+				ArrayList<Object> list = (ArrayList<Object>) object;
+				ArrayList<Object> versionInfoList = (ArrayList<Object>) versionObject;
+				for (Object oldMap : versionInfoList) {
+					Map<String, String> map = (Map) oldMap;
+					CommandPackageVersion verInfo = new CommandPackageVersion();
+					verInfo.setAgentId(result.getAgentId());
+					verInfo.setTaskId(result.getCommandExecution().getCommand().getTask().getId());
+					verInfo.setCreateDate(new Date());
+					verInfo.setCommand(map.get("c").toString());
+					verInfo.setPackageName(map.get("p").toString());
+					verInfo.setPackageVersion(map.get("v").toString());
+
+					pluginDbService.save(verInfo);
+				}
+
+				for (Object oldMap : list) {
+					Map<String, String> map = (Map) oldMap;
+					CommandExecutionStatistics item = new CommandExecutionStatistics();
+					item.setCommand(map.get("c").toString());
+					item.setUser(map.get("u").toString());
+					Float processTime = Float.parseFloat(map.get("p").toString());
+					item.setProcessTime(processTime);
+					String currentYearString = Integer.toString(Calendar.getInstance().get(Calendar.YEAR));
+					DateFormat formatter = new SimpleDateFormat("E MMM dd HH:mm:ss yyyy");
+					item.setProcessStartDate(
+							(Date) formatter.parse(map.get("s").toString() + ":00 " + currentYearString));
+					item.setAgentId(result.getAgentId());
+					item.setTaskId(result.getCommandExecution().getCommand().getTask().getId());
+					item.setIsActive("1");
+					item.setCreateDate(new Date());
+					item.setCommandExecutionId(result.getCommandExecution().getId());
+
+					Query query = entityManager.createQuery(
+							"UPDATE CommandExecutionStatistics ces SET ces.isActive ='0' WHERE ces.agentId = :agentId AND ces.command = :command AND ces.user = :user AND ces.taskId <> :taskId");
+					query.setParameter("agentId", item.getAgentId());
+					query.setParameter("taskId", item.getTaskId());
+					query.setParameter("command", item.getCommand());
+					query.setParameter("user", item.getUser());
+					query.executeUpdate();
+					pluginDbService.save(item);
+				}
+			}
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -103,88 +173,6 @@ public class GetExecutionInfoCommand implements ICommand, ITaskAwareCommand {
 
 	public void setTaskDao(ITaskDao taskDao) {
 		this.taskDao = taskDao;
-	}
-
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@Override
-	public void onTaskUpdate(ICommandExecutionResult result) {
-		if (result != null && result.getCommandExecution() != null && result.getCommandExecution().getCommand() != null
-				&& result.getCommandExecution().getCommand().getTask() != null
-				&& result.getCommandExecution().getCommand().getTask().getCommandClsId() != null
-				&& result.getCommandExecution().getCommand().getTask().getPlugin() != null
-				&& result.getCommandExecution().getCommand().getTask().getPlugin().getName() != null
-				&& result.getCommandExecution().getCommand().getTask().getCommandClsId().toUpperCase()
-						.equals("GET_EXECUTION_INFO")
-				&& "PACKAGE-MANAGER".equals(
-						result.getCommandExecution().getCommand().getTask().getPlugin().getName().toUpperCase())) {
-
-			ICommandExecutionResult res = getCommandDao().findExecutionResult(result.getId());
-			byte[] data  = null;
-			if(res != null)
-				data = res.getResponseData();
-			Map<String, Object> responseData;
-			try {
-				responseData = new ObjectMapper().readValue(data, 0, data.length,
-						new TypeReference<HashMap<String, Object>>() {
-						});
-				if (responseData != null && !responseData.isEmpty()
-						&& responseData.containsKey("commandExecutionInfoList")
-						&& responseData.containsKey("versionList")) {
-					Object object = responseData.get("commandExecutionInfoList");
-					Object versionObject = responseData.get("versionList");
-					ArrayList<Object> list = (ArrayList<Object>) object;
-					ArrayList<Object> versionInfoList = (ArrayList<Object>) versionObject;
-					for (Object oldMap : versionInfoList) {
-						Map<String, String> map = (Map) oldMap;
-						CommandPackageVersion verInfo = new CommandPackageVersion();
-						verInfo.setAgentId(result.getAgentId());
-						verInfo.setTaskId(result.getCommandExecution().getCommand().getTask().getId());
-						verInfo.setCreateDate(new Date());
-						verInfo.setCommand(map.get("c").toString());
-						verInfo.setPackageName(map.get("p").toString());
-						verInfo.setPackageVersion(map.get("v").toString());
-
-						pluginDbService.save(verInfo);
-					}
-
-					for (Object oldMap : list) {
-						Map<String, String> map = (Map) oldMap;
-						CommandExecutionStatistics item = new CommandExecutionStatistics();
-						item.setCommand(map.get("c").toString());
-						item.setUser(map.get("u").toString());
-						Float processTime = Float.parseFloat(map.get("p").toString());
-						item.setProcessTime(processTime);
-						String currentYearString = Integer.toString(Calendar.getInstance().get(Calendar.YEAR));
-						DateFormat formatter = new SimpleDateFormat("E MMM dd HH:mm:ss yyyy");
-						item.setProcessStartDate(
-								(Date) formatter.parse(map.get("s").toString() + ":00 " + currentYearString));
-						item.setAgentId(result.getAgentId());
-						item.setTaskId(result.getCommandExecution().getCommand().getTask().getId());
-						item.setIsActive("1");
-						item.setCreateDate(new Date());
-						item.setCommandExecutionId(result.getCommandExecution().getId());
-
-						Query query = entityManager.createQuery(
-								"UPDATE CommandExecutionStatistics ces SET ces.isActive ='0' WHERE ces.agentId = :agentId AND ces.command = :command AND ces.user = :user AND ces.taskId <> :taskId");
-						query.setParameter("agentId", item.getAgentId());
-						query.setParameter("taskId", item.getTaskId());
-						query.setParameter("command", item.getCommand());
-						query.setParameter("user", item.getUser());
-						query.executeUpdate();
-						pluginDbService.save(item);
-					}
-				}
-			} catch (JsonParseException e) {
-				e.printStackTrace();
-			} catch (JsonMappingException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-
-		}
 	}
 
 	public EntityManager getEntityManager() {
